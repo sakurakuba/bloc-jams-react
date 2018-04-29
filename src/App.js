35ba34f84b808cbea39d99f8ac2fdda12c424786
sakurakuba <ksulaymanov@gmail.com>
2018-04-29 10:28:20 -0400

    components-library: completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Link} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
@@ -12,7 +11,7 @@ class App extends Component {
       <div className="App">
         <header>
           <nav>
-            <Link to='/'>Landing</Link>
+            <Link to='/'>Landing </Link>
             <Link to='/library'>Library</Link>
             </nav>
           <h1>Bloc Jams</h1> 
@@ -20,7 +19,7 @@ class App extends Component {
         <main>
           <Route exact path="/" Component={Landing} />
           <Route path="/library" Component={Library} />
-          <Route path="/album" Component={Album} />
+          <Route path="/album/:slug" Component={Album} />
         </main>
       </div>
     );
