9b6f4f579fdc0d214543bbf058567871186c19ba
sakurakuba <ksulaymanov@gmail.com>
2018-05-07 21:48:01 -0400

    player-buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
     constructor(props) {
@@ -44,6 +45,14 @@ class Album extends Component {
         }
     }
 
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max(0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
+
     render() {
         return (
             <section className="album">
@@ -73,13 +82,19 @@ class Album extends Component {
               <span className="ion-pause"></span>
             </button>
           </td>
-            <td>{song.title}</td>
-            <td>{song.duration}</td>
+            <td className="song-title">{song.title}</td>
+            <td className="song-duration">{song.duration}</td>
             </tr>
             )
            }
                 </tbody>
                 </table>
+                <PlayerBar 
+                    isPlaying={this.state.isPlaying} 
+                    currentSong={this.state.currentSong} 
+                    handleSongClick={ () => this.handleSongClick(this.state.currentSong) }
+                    handlePrevClick={ () => this.handlePrevClick() }
+                    />
             </section>
         );
     }
